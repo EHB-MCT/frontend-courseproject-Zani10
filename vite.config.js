544a054d2953c6e438c4516dadf934c47fbac792
Zani Dobruna <80400239+Zani10@users.noreply.github.com>
2024-08-12 23:50:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frontend-courseproject-Zani10/',
+  base: '/frontend-courseproject-Zani10',
   plugins: [react()],
 });
